98c2fd9597227410f8f335f7211e6785212665a6
Mike Thiem <mike.j.thiem@gmail.com>
2025-02-02 08:40:15 -0800

    cleanup

--- monitor/consumers.py
+++ monitor/consumers.py
@@ -6,8 +6,16 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class MonitorConsumer(AsyncWebsocketConsumer):
+    """MonitorConsumer is used to send WS messages containing the most recent audio level measured by a given monitor device."""
     async def connect(self):
         self.device_id = self.scope['url_route']['kwargs']['device_id']
+        
+        self.channel_layer = get_channel_layer()
+        if self.channel_layer is None:
+            logger.error("Failed to get channel layer")
+            await self.close()
+            return
+        
         self.room_group_name = f'monitor_{self.device_id}'
         logger.debug(f"New connection attempt for device {self.device_id}")
 
@@ -20,12 +28,15 @@ class MonitorConsumer(AsyncWebsocketConsumer):
         logger.info(f"WebSocket connected for device {self.device_id}")
 
 
-    async def disconnect(self, close_code):
+    async def disconnect(self, code):
         logger.debug(f"Disconnecting from group: {self.room_group_name}")
+        if self.channel_layer is not None:
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        else:
+            logger.error("Channel layer is None, cannot discard group")
 
     async def monitor_message(self, event):
         logger.debug(f"Consumer received event to broadcast: {event}")
